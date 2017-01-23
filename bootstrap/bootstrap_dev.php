127e409dbd38191d1161d33a8a1b6d29b0f97948
bellorchid <zhongyunchang@yeah.net>
2017-01-23 13:15:31 +0800

    init

---
+++ bootstrap/bootstrap_dev.php
@@ -0,0 +1,14 @@
+<?php
+
+use Sliex\Application;
+use Silex\Application;
+
+date_default_timezone_set('Asia/Shanghai');
+
+require $root . '/vendor/autoload.php';
+
+$config = require _DIR_ .'../../config/config_dev.php';
+
+
+$app = new Application($config);
+$app->run();
